5714c68ba7c7b2ae1898805b38a8d367974b7d27
Nairio <nairik@mail.ru>
2020-05-18 08:15:48 +0300

    Normalize 1

--- front/src/index.js
+++ front/src/index.js
@@ -5,7 +5,6 @@ import "./modules/features/css"
 import * as serviceWorker from './serviceWorker';
 import App from "./App";
 
-
 ReactDOM.render(<App/>, document.getElementById('root'));
 
 serviceWorker.unregister();
