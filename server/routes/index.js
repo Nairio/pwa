7d980df76796954c83e3493964677ed335c0307d
Nairio <nairik@mail.ru>
2020-05-24 11:17:34 +0300

    Normalize

--- server/routes/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = (app, client) => {
-    require('./getAll')(app, client);
-    require('./change')(app, client);
-    require('./delete')(app, client);
-    require('./add')(app, client);
-    require('./generator')(app, client);
-    require('./auth')(app, client);
-};
