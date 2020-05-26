df4faeaa7a1a2a0b1b7b4be55ccfbbacfc30eaf7
Nairio <nairik@mail.ru>
2020-05-26 11:01:12 +0300

    DBVirtualLis

--- src/modules/pages/teacher/contact.js
+++ src/modules/pages/teacher/contact.js
@@ -1,79 +1,39 @@
 import React from 'react';
-import VirtualList from "../../templates/virtual-list";
-import FullScreen from "../../templates/fullscreen";
-import {FlexBox, FlexScroll} from "../../templates/flex";
+import {FlexBox} from "../../templates/flex";
 import Header from "../../header/header";
 import HeaderTitle from "../../header/header-title";
 import AddIcon from '@material-ui/icons/Add';
 import {Avatar, ListItem, ListItemAvatar, List} from "@material-ui/core";
-import IconButton from "@material-ui/core/IconButton";
 
-import {Field, Form} from "../../templates/form";
-import Search from "../../header/search";
-import {DB} from "../../features/firebase";
+import DBVirtualList from "../../templates/db-virtual-list";
+import FabRightBottom from "../../templates/fab-right-bottom";
+import DeleteForeverIcon from "@material-ui/core/SvgIcon/SvgIcon";
 
 export default class Contact extends React.Component {
-    constructor(props, context) {
-        super(props, context);
-        this.state = {data: false, index: 0, open: false, isAdd: false, item: {}};
-        this.onClose = this.onClose.bind(this);
-        this.onCreate = this.onCreate.bind(this);
-        this.onEdit = this.onEdit.bind(this);
-    }
-
-    onClose() {
-        this.setState({open: false})
-    }
-
-    onCreate() {
-        this.setState({open: true, isAdd: true, item: {}})
-    }
-
-    onEdit(item) {
-        this.setState({open: true, isAdd: false, item})
-    }
-
-    componentDidMount() {
-        this.DB = DB("teacher.contacts", ({data, index}) => this.setState({open: false, data, index, modified: new Date()}));
-        this.DB.load();
-    }
-
-    componentWillUnmount() {
-        this.DB.close()
-    }
-
-    render() {
-        const {data, modified, index, open, isAdd, item} = this.state;
+     render() {
         return (
             <FlexBox>
                 <Header>
                     <HeaderTitle align="left">{this.props.title}</HeaderTitle>
-                    <Search onChange={term => this.DB.filter(term)}/>
                 </Header>
                 <FlexBox>
-                    <FullScreen open={open} title={item.title} onClose={this.onClose}>
-                        <FlexScroll>
-                            <FlexBox middle center>
-                                <Form item={item} onSubmit={item => isAdd ? this.DB.add(item) : this.DB.change(item)}>
-                                    <Field id="photo" type="text" title="Фотография"/>
-                                    <Field id="firstname" type="text" title="Имя"/>
-                                    <Field id="lastname" type="text" title="Фамилия"/>
-                                    <Field id="patronymic" type="text" title="Отчество"/>
-                                </Form>
-                            </FlexBox>
-                        </FlexScroll>
-                    </FullScreen>
-
-                    {data && data.length < 1 && <FlexBox middle center><IconButton color="secondary" onClick={this.onCreate}><AddIcon/></IconButton></FlexBox>}
-
-                    <VirtualList
-                        onPull={() => this.DB.load()}
-                        data={data}
-                        index={index}
-                        modified={modified}
-                        divider="inset"
-                        template={(item) => (
-                            <ListItem button alignItems="flex-start" onClick={() => this.onEdit(item)}>
+                    <DBVirtualList
+                        single={true}
+                        DBPath="teacher.contacts"
+                        fields={[
+                            {id: "photo", title: "Фотография", type: "text"},
+                            {id: "firstname", title: "Имя", type: "text"},
+                            {id: "lastname", title: "Фамилия", type: "text"},
+                            {id: "patronymic", title: "Отчество", type: "text"},
+                        ]}
+                        createButton={(onCreate) => (
+                            <FabRightBottom color="secondary" onClick={onCreate}><AddIcon/></FabRightBottom>
+                        )}
+                        deleteButton={(onDelete) => (
+                            <FabRightBottom size="small" color="secondary" onClick={onDelete}><DeleteForeverIcon/></FabRightBottom>
+                        )}
+                        template={(item, onEdit) => (
+                            <ListItem button alignItems="flex-start" onClick={onEdit}>
                                 {item.photo && <ListItemAvatar><Avatar src={item.photo}/></ListItemAvatar>}
                                 <List>
                                     <ListItem>Имя: {item.firstname}</ListItem>
--- src/modules/pages/teacher/diploms.js
+++ src/modules/pages/teacher/diploms.js
@@ -1,84 +1,45 @@
 import React from 'react';
-import VirtualList from "../../templates/virtual-list";
-import FullScreen from "../../templates/fullscreen";
-import {FlexBox, FlexScroll} from "../../templates/flex";
+import {FlexBox} from "../../templates/flex";
 import Header from "../../header/header";
 import HeaderTitle from "../../header/header-title";
-import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 import AddIcon from '@material-ui/icons/Add';
-import {Avatar, ListItem, ListItemAvatar, ListItemText} from "@material-ui/core";
-import IconButton from "@material-ui/core/IconButton";
-import EditIcon from '@material-ui/icons/Edit';
+import {Avatar, ListItem, ListItemAvatar, List} from "@material-ui/core";
+
+import DBVirtualList from "../../templates/db-virtual-list";
 import FabRightBottom from "../../templates/fab-right-bottom";
-import {Field, Form} from "../../templates/form";
-import Search from "../../header/search";
-import {DB} from "../../features/firebase";
+import DeleteForeverIcon from "@material-ui/core/SvgIcon/SvgIcon";
 
 export default class Diploms extends React.Component {
-    constructor(props, context) {
-        super(props, context);
-        this.state = {data: false, index: 0, open: false, isAdd: false, item: {}};
-        this.onClose = this.onClose.bind(this);
-        this.onCreate = this.onCreate.bind(this);
-        this.onEdit = this.onEdit.bind(this);
-    }
-
-    onClose() {
-        this.setState({open: false})
-    }
-
-    onCreate() {
-        this.setState({open: true, isAdd: true, item: {}})
-    }
-
-    onEdit(item) {
-        this.setState({open: true, isAdd: false, item})
-    }
-
-    componentDidMount() {
-        this.DB = DB("teacher", ({data, index}) => this.setState({open: false, data, index, modified: new Date()}));
-        this.DB.load();
-    }
-
-    componentWillUnmount() {
-        this.DB.close()
-    }
-
     render() {
-        const {data, modified, index, open, isAdd, item} = this.state;
         return (
             <FlexBox>
                 <Header>
                     <HeaderTitle align="left">{this.props.title}</HeaderTitle>
-                    <Search onChange={term => this.DB.filter(term)}/>
-                    <IconButton color="secondary" onClick={this.onCreate}><AddIcon/></IconButton>
                 </Header>
                 <FlexBox>
-                    <FullScreen open={open} title={item.title} onClose={this.onClose}>
-                        <FlexScroll>
-                            <FlexBox middle center>
-                                <Form item={item} onSubmit={item => isAdd ? this.DB.add(item) : this.DB.change(item)}>
-                                    <Field id="img" type="image" title="Иконка"/>
-                                    <Field id="_id" type="disabled" title="Идентификатор"/>
-                                    <Field id="sort" type="disabled" title="Сортировка"/>
-                                    <Field id="title" type="text" title="Название"/>
-                                    <Field id="text" type="text" title="Текст"/>
-                                </Form>
-                            </FlexBox>
-                        </FlexScroll>
-                        <FabRightBottom size="small" hide={isAdd} color="secondary" onClick={() => this.DB.delete(item)}><DeleteForeverIcon/></FabRightBottom>
-                    </FullScreen>
-                    <VirtualList
-                        onPull={() => this.DB.load()}
-                        data={data}
-                        index={index}
-                        modified={modified}
-                        divider="inset"
-                        template={(item) => (
-                            <ListItem button alignItems="flex-start">
-                                {item.img && <ListItemAvatar><Avatar src={item.img}/></ListItemAvatar>}
-                                <ListItemText inset={!item.img} primary={`${item.title} ${item.text}`} secondary={item._id}/>
-                                <IconButton onClick={() => this.onEdit(item)}><EditIcon/></IconButton>
+                    <DBVirtualList
+                        single={false}
+                        DBPath="teacher.diploms"
+                        fields={[
+                            {id: "photo", title: "Фотография", type: "text"},
+                            {id: "firstname", title: "Имя", type: "text"},
+                            {id: "lastname", title: "Фамилия", type: "text"},
+                            {id: "patronymic", title: "Отчество", type: "text"},
+                        ]}
+                        createButton={(onCreate) => (
+                            <FabRightBottom color="secondary" onClick={onCreate}><AddIcon/></FabRightBottom>
+                        )}
+                        deleteButton={(onDelete) => (
+                            <FabRightBottom size="small" color="secondary" onClick={onDelete}><DeleteForeverIcon/></FabRightBottom>
+                        )}
+                        template={(item, onEdit) => (
+                            <ListItem button alignItems="flex-start" onClick={onEdit}>
+                                {item.photo && <ListItemAvatar><Avatar src={item.photo}/></ListItemAvatar>}
+                                <List>
+                                    <ListItem>Имя: {item.firstname}</ListItem>
+                                    <ListItem>Фамилия: {item.lastname}</ListItem>
+                                    <ListItem>Отчество: {item.patronymic}</ListItem>
+                                </List>
                             </ListItem>
                         )}
                     />
