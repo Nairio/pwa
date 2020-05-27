e158c1ef996b40fe094848c56f42e41744e4f3a3
Nairio <nairik@mail.ru>
2020-05-27 07:14:42 +0300

    norm

---
+++ src/modules/pages/teacher/contacts.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {Avatar, ListItem, ListItemAvatar, List} from "@material-ui/core";
+
+import DBVirtualList from "../../templates/db-virtual-list";
+
+export default class Contacts extends React.Component {
+     render() {
+        return (
+            <DBVirtualList
+                single={true}
+                DBPath="teacher.contacts"
+                fields={[
+                    {id: "photo", title: "Фотография", type: "text"},
+                    {id: "firstname", title: "Имя", type: "text"},
+                    {id: "lastname", title: "Фамилия", type: "text"},
+                    {id: "patronymic", title: "Отчество", type: "text"},
+                ]}
+                template={(item, onEdit) => (
+                    <ListItem button alignItems="flex-start" onClick={onEdit}>
+                        {item.photo && <ListItemAvatar><Avatar src={item.photo}/></ListItemAvatar>}
+                        <List>
+                            <ListItem>Имя: {item.firstname}</ListItem>
+                            <ListItem>Фамилия: {item.lastname}</ListItem>
+                            <ListItem>Отчество: {item.patronymic}</ListItem>
+                        </List>
+                    </ListItem>
+                )}
+            />
+        )
+    }
+}
