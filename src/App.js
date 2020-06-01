dc49d22da82023ae07168c1a975f3beff5b8f1be
Nairio <nairik@mail.ru>
2020-06-01 14:16:59 +0300

    sweepable

--- src/App.js
+++ src/App.js
@@ -17,17 +17,10 @@ import ChatIcon from '@material-ui/icons/Chat';
 import MonetizationOnIcon from '@material-ui/icons/MonetizationOn';
 import DateRangeIcon from '@material-ui/icons/DateRange';
 import DvrIcon from '@material-ui/icons/Dvr';
-import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
-import PublicIcon from '@material-ui/icons/Public';
 import GroupIcon from '@material-ui/icons/Group';
-import AccessibilityNewIcon from '@material-ui/icons/AccessibilityNew';
 import LockIcon from '@material-ui/icons/Lock';
 import LockOpenIcon from '@material-ui/icons/LockOpen';
-import VerifiedUserIcon from '@material-ui/icons/VerifiedUser';
 import Contacts from "./modules/pages/parents/contacts";
-import Diploms from "./modules/pages/parents/diploms";
-import Experience from "./modules/pages/parents/experience";
-import Achievements from "./modules/pages/parents/achievements";
 import Courses from "./modules/pages/parents/courses";
 import Groups from "./modules/pages/parents/groups";
 import Students from "./modules/pages/parents/students";
@@ -35,14 +28,6 @@ import Calendar from "./modules/pages/calendar/calendar";
 import Chat from "./modules/pages/chat/chat";
 import Reports from "./modules/pages/reports/reports";
 import Hslide from "./modules/pages/parents/hslide";
-import Places from "./modules/pages/parents/places";
-import AccountBalanceIcon from '@material-ui/icons/AccountBalance';
-import CategoryIcon from '@material-ui/icons/Category';
-import Subjects from "./modules/pages/parents/subjects";
-import HowToRegIcon from '@material-ui/icons/HowToReg';
-import Teachers from "./modules/pages/parents/teachers";
-import Levels from "./modules/pages/parents/levels";
-import Parents from "./modules/pages/parents/parents";
 import ChildCareIcon from '@material-ui/icons/ChildCare';
 import HistoryIcon from '@material-ui/icons/History';
 import History from "./modules/pages/parents/history";
@@ -88,10 +73,10 @@ class App extends React.Component {
                         <Page title="Контакты" icon={<AccountBoxIcon/>} component={Contacts}/>
                     </SwipeableBottom>
                 </Page>
+                <Page disabled={!email} title="Студенты" icon={<ChildCareIcon/>} path="/students/" component={Students}/>
+
                 <Divider/>
                 <Page disabled={!email} title="Курсы" icon={<DvrIcon/>} path="/courses/" component={Courses}/>
-                <Page disabled={!email} title="Группы" icon={<GroupIcon/>} path="/groups/" component={Groups}/>
-                <Page disabled={!email} title="Студенты" icon={<ChildCareIcon/>} path="/students/" component={Students}/>
                 <Page disabled={!email} title="История" icon={<HistoryIcon/>} path="/history/" component={History}/>
 
                 <Divider/>
