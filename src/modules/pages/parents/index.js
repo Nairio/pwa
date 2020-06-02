08caa85ffe41378a6dd6e685471a095373904eb9
Nairio <nairik@mail.ru>
2020-06-02 22:33:00 +0300

    sweepable

--- src/modules/pages/parents/index.js
+++ src/modules/pages/parents/index.js
@@ -1,32 +1,20 @@
 import React from 'react';
-import {FlexBox, FlexScroll} from "../../templates/flex";
-import Menu from "../../menu/menu";
-import HeaderTitle from "../../header/header-title";
-import Header from "../../header/header";
-import {Calendar, momentLocalizer} from 'react-big-calendar'
-import moment from 'moment';
-
-
-const localizer = momentLocalizer(moment)
+import {FlexBox} from "../../templates/flex";
+import { DatePicker } from "@material-ui/pickers";
 
 
 export default class Index extends React.Component {
     render() {
         return (
             <FlexBox>
-                <Header>
-                    <Menu/>
-                    <HeaderTitle>{this.props.title}</HeaderTitle>
-                </Header>
-                <FlexScroll>
-                    <Calendar
-                        localizer={localizer}
-                        events={myEventsList}
-                        startAccessor="start"
-                        endAccessor="end"
-                        style={{ height: 500 }}
-                    />
-                </FlexScroll>
+                <DatePicker
+                    autoOk
+                    orientation="landscape"
+                    variant="static"
+                    openTo="date"
+                    value={date}
+                    onChange={changeDate}
+                />
             </FlexBox>
         )
     }
