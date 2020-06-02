3a44065929adfc66efc119677012754d0bcf5bef
Nairio <nairik@mail.ru>
2020-06-03 00:16:07 +0300

    sweepable

--- src/modules/pages/parents/index.js
+++ src/modules/pages/parents/index.js
@@ -1,20 +1,46 @@
 import React from 'react';
-import {FlexBox} from "../../templates/flex";
-import { DatePicker } from "@material-ui/pickers";
+import {FlexBox, FlexScroll} from "../../templates/flex";
+import Menu from "../../menu/menu";
+import HeaderTitle from "../../header/header-title";
+import Header from "../../header/header";
+import ruLocale from "date-fns/locale/ru";
+
+import {useState} from "react";
+import {DatePicker, MuiPickersUtilsProvider} from "@material-ui/pickers";
+import DateFnsUtils from '@date-io/date-fns';
+
+const StaticDatePicker = () => {
+    const [date, changeDate] = useState(new Date());
+
+    return (
+        <MuiPickersUtilsProvider  locale={ruLocale} utils={DateFnsUtils}>
+            <DatePicker
+                autoOk
+                orientation="landscape"
+                variant="static"
+                openTo="date"
+                value={date}
+                onChange={changeDate}
+            />
+        </MuiPickersUtilsProvider>
+    );
+};
 
 
 export default class Index extends React.Component {
     render() {
         return (
             <FlexBox>
-                <DatePicker
-                    autoOk
-                    orientation="landscape"
-                    variant="static"
-                    openTo="date"
-                    value={date}
-                    onChange={changeDate}
-                />
+                <Header>
+                    <Menu/>
+                    <HeaderTitle>{this.props.title}</HeaderTitle>
+                </Header>
+                <FlexScroll>
+                    <FlexBox center middle>
+                        <h1>{this.props.title}</h1>
+                        <StaticDatePicker/>
+                    </FlexBox>
+                </FlexScroll>
             </FlexBox>
         )
     }
