75d4a484b650dbbbfeaff028a91b03a56d7b3bf8
Nairio <nairik@mail.ru>
2020-06-14 03:12:03 +0300

    calendar

--- src/modules/pages/parents/index.js
+++ src/modules/pages/parents/index.js
@@ -12,9 +12,8 @@ export default class Index extends React.Component {
         this.onScheduler = this.onScheduler.bind(this);
     }
 
-    onScheduler(scheduler){
-        console.log(scheduler);
-        scheduler.init('scheduler_here',new Date(2017,5,30),"year");
+    onScheduler(scheduler, container){
+        scheduler.init(container, new Date(2017,5,30), "year");
     }
 
     render() {
@@ -26,7 +25,22 @@ export default class Index extends React.Component {
                 </Header>
                 <FlexScroll>
                     <FlexBox center top>
-                        <Scheduler onScheduler={this.onScheduler}/>
+                        <Scheduler onScheduler={this.onScheduler}>
+                            <div className="dhx_cal_navline">
+                                <div className="dhx_cal_prev_button">&nbsp;</div>
+                                <div className="dhx_cal_next_button">&nbsp;</div>
+                                <div className="dhx_cal_today_button"></div>
+                                <div className="dhx_cal_date"></div>
+                                <div className="dhx_cal_tab" name="day_tab" style="right:204px;"></div>
+                                <div className="dhx_cal_tab" name="week_tab" style="right:140px;"></div>
+                                <div className="dhx_cal_tab" name="year_tab" style="right:280px;"></div>
+                                <div className="dhx_cal_tab" name="month_tab" style="right:76px;"></div>
+                            </div>
+                            <div className="dhx_cal_header">
+                            </div>
+                            <div className="dhx_cal_data">
+                            </div>
+                        </Scheduler>
                     </FlexBox>
                 </FlexScroll>
             </FlexBox>
