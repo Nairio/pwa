005fbf214e0c6337df578576e21c4d544e06aaf2
Nairio <nairik@mail.ru>
2020-06-14 03:23:42 +0300

    calendar

--- src/modules/pages/parents/index.js
+++ src/modules/pages/parents/index.js
@@ -29,12 +29,12 @@ export default class Index extends React.Component {
                             <div className="dhx_cal_navline">
                                 <div className="dhx_cal_prev_button">&nbsp;</div>
                                 <div className="dhx_cal_next_button">&nbsp;</div>
-                                <div className="dhx_cal_today_button"></div>
-                                <div className="dhx_cal_date"></div>
-                                <div className="dhx_cal_tab" name="day_tab" style="right:204px;"></div>
-                                <div className="dhx_cal_tab" name="week_tab" style="right:140px;"></div>
-                                <div className="dhx_cal_tab" name="year_tab" style="right:280px;"></div>
-                                <div className="dhx_cal_tab" name="month_tab" style="right:76px;"></div>
+                                <div className="dhx_cal_today_button"/>
+                                <div className="dhx_cal_date"/>
+                                <div className="dhx_cal_tab" name="day_tab" style="right:204px;"/>
+                                <div className="dhx_cal_tab" name="week_tab" style="right:140px;"/>
+                                <div className="dhx_cal_tab" name="year_tab" style="right:280px;"/>
+                                <div className="dhx_cal_tab" name="month_tab" style="right:76px;"/>
                             </div>
                             <div className="dhx_cal_header">
                             </div>
