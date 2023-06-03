3282f62b3f221781c20ed1fea2b3c30adee34984
nairio <nairik@mail.ru>
2023-06-03 18:37:15 +0300

    react-scripts

--- src/modules/templates/virtual-list.js
+++ src/modules/templates/virtual-list.js
@@ -16,20 +16,24 @@ export default class VirtualList extends React.Component {
         this.setItem = {};
         this.itemCount = 0;
     }
+
     renderRow({style, index}) {
         const {data, divider} = this.props;
 
-        const [item, setItem] = React.useState(data[index]);
-        this.setItem[index] = setItem;
-        item !== data[index] && setItem(data[index]);
+        return (() => {
+            const [item, setItem] = React.useState(data[index]);
+            this.setItem[index] = setItem;
+            item !== data[index] && setItem(data[index]);
 
-        return (
-            <div style={style}>
-                <this.props.template {...item}/>
-                <Divider variant={divider} component="div"/>
-            </div>
-        )
+            return (
+                <div style={style}>
+                    <this.props.template {...item}/>
+                    <Divider variant={divider} component="div"/>
+                </div>
+            )
+        })()
     }
+
     componentDidUpdate(prevProps, prevState, snapshot) {
         const {template, divider, data, modified, index} = this.props;
 
@@ -48,25 +52,41 @@ export default class VirtualList extends React.Component {
             this.resize();
         }
     }
+
     componentDidMount() {
         this.resize();
     }
+
     componentWillUnmount() {
         window.removeEventListener("resize", this.resize);
     }
+
     resize() {
         const fix = this.listRef.current._outerRef;
-        const itemSize = (r => !r ? 1 : ((h, i) => {r.style.height = null; i = r.clientHeight; r.style.height = h; return i})(r.style.height))(fix.children[0].children[0]);
-        const height = (f => ((d, i) => {f.style.display = "none"; i = f.parentElement.clientHeight; f.style.display = d; return i})(f.style.display))(fix);
+        const itemSize = (r => !r ? 1 : ((h, i) => {
+            r.style.height = null;
+            i = r.clientHeight;
+            r.style.height = h;
+            return i
+        })(r.style.height))(fix.children[0].children[0]);
+        const height = (f => ((d, i) => {
+            f.style.display = "none";
+            i = f.parentElement.clientHeight;
+            f.style.display = d;
+            return i
+        })(f.style.display))(fix);
 
         this.setState({height, itemSize});
     }
+
     render() {
         const {height, itemSize} = this.state;
 
         return (
-            <FlexPullToRefresh canRefresh={() => this.listRef.current._outerRef.scrollTop === 0} onRefresh={this.props.onPull}>
-                <FixedSizeList ref={this.listRef} width="100%" height={height} itemSize={itemSize} itemCount={this.props.data.length}>
+            <FlexPullToRefresh canRefresh={() => this.listRef.current._outerRef.scrollTop === 0}
+                               onRefresh={this.props.onPull}>
+                <FixedSizeList ref={this.listRef} width="100%" height={height} itemSize={itemSize}
+                               itemCount={this.props.data.length}>
                     {this.renderRow}
                 </FixedSizeList>
             </FlexPullToRefresh>
